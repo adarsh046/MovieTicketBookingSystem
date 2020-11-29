e4da51c4010167e5ff5d58d976db2c4719ef4bc8
Adarsh Gupta <48509115+adarsh046@users.noreply.github.com>
2020-11-29 16:16:08 +0530

    Add files via upload

---
+++ MovieTicketBookingSystem-main/src/Signup/Drag.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Signup;
+
+import java.awt.event.MouseEvent;
+import keeptoo.KGradientPanel;
+
+/**
+ *
+ * @author ADARSH
+ */
+public class Drag {
+
+    Drag(KGradientPanel kGradientPanel1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void onPress(MouseEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void moveWindow(MouseEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
